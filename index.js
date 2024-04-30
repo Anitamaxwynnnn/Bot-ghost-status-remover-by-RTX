295cef025fc0b5b191982a5a476290a5959388dd
Anitamaxwynnnn <157937169+Anitamaxwynnnn@users.noreply.github.com>
2024-04-29 23:44:37 -0800

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Nopee","Kite 2.0 🎧","Join Nopee Server 💨","Tarz","Filow's Tambayan","ContestDaw","Pawsomepals"];
+const statusMessages = ["./."];
 
 
 let currentIndex = 0;
@@ -74,7 +74,7 @@ function updateStatusAndSendMessages() {
 
   client.user.setPresence({
     activities: [{ name: currentStatus, type: ActivityType.Custom}],
-    status: 'dnd',
+    status: 'idle',
   });
 
   
